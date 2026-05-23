2fcb8ac6d81d1e98ffc4d16dbc514e92cc253829
kauboi007 <kaushikmugundan2019@gmail.com>
2026-05-23 10:13:00 +0530

    added FF factors and preparing for regression model

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from statsmodels.regression.rolling import RollingOLS
-#import pandas_datareader.data as web
 import pandas as pd
 import matplotlib.pyplot as plt
 import statsmodels.api as sm
@@ -13,6 +12,7 @@ import pandas_ta
 
 wikiurl="https://en.wikipedia.org/wiki/NIFTY_50"
 CACHE_FILE= "nifty50data.parquet"
+outlier_cutoff=0.005
 
 if os.path.exists(CACHE_FILE):
     df=pd.read_parquet(CACHE_FILE)
@@ -48,4 +48,30 @@ def compute_macd(close):
     return macd.sub(macd.mean()).div(macd.std())
 df['macd']=df.groupby(level=1,group_keys=False)['close'].apply(compute_macd)
 df['rs_vol']=(df['volume']*df['close'])/1e6
-print(df)
+
+lastcols=[c for c in df.columns.unique(0) if c not in ['rs_vol', 'open','volume','high','low']]
+features=df.unstack()[lastcols].resample('ME').last().stack('ticker')
+vol=df.unstack('ticker')['rs_vol'].resample('ME').mean().stack('ticker').to_frame('rs_vol')
+data=pd.concat([features,vol],axis=1).dropna()
+
+data['rs_vol']=data['rs_vol'].unstack('ticker').rolling(5*12).mean().stack()
+data=data.drop(['rs_vol'],axis=1)
+
+def compute_returns(df):
+    lags=[1,2,3,6,9,12]
+    for lag in lags:
+        df[f'return_{lag}m']=df['close'].pct_change(lag).pipe(lambda x: x.clip(lower=x.quantile(outlier_cutoff),upper=x.quantile(1-outlier_cutoff))).add(1).pow(1/lag).sub(1)
+    return df
+
+data=data.groupby(level=1,group_keys=False).apply(compute_returns).dropna()
+data=data.drop('close', axis=1) 
+
+factordata=pd.read_csv("FFdata.csv",parse_dates=['Date']).drop('RF',axis=1)
+factordata = factordata.set_index('Date')
+factordata=factordata.resample('ME').last().div(100)
+factordata.index.name='date'
+factordata=factordata.join(data['return_1m']).sort_index()
+obs=(factordata.groupby(level=1).size())
+valid=obs[obs>=10]
+factordata=factordata[factordata.index.get_level_values('ticker').isin(valid.index)]
+#print(factordata)
